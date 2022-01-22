852da7b7de9074b8cefe7ac1e25a6e3eee848ad9
faculdados <caio.alencikas@gmail.com>
2022-01-22 12:03:21 -0300

    Testando user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-
 import Header from './components/Header'
 import Location from './components/Location'
-
 import locationsData from './data'
 
 export default function App() {
